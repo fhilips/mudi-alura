aaa243e9bf4246e3752581f18347fa4cdbd55002
Filipe dos Santos <fhilips.souza@gmail.com>
2020-10-01 20:08:25 -0300

    Páginas de listagem e cadastro

---
+++ src/main/java/br/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,56 @@
+package br.com.alura.mvc.mudi.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+import br.com.alura.mvc.mudi.model.StatusPedido;
+
+public class RequisicaoNovoPedido {
+
+	@NotBlank // NotBlank.requisicaoNovoPedido.nomeProduto = não pode estar em branco
+	private String nomeProduto;
+	
+	@NotBlank
+	private String urlImagem;
+	
+	@NotBlank
+	private String urlProduto;
+	private String descricao;
+	
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Pedido toPedido() {
+		
+		Pedido pedido = new Pedido();
+		pedido.setDescricao(descricao);
+		pedido.setNomeProduto(nomeProduto);
+		pedido.setUrlImagem(urlImagem);
+		pedido.setUrlProduto(urlProduto);
+		pedido.setStatus(StatusPedido.AGUARDANDO);
+		return pedido;
+	}
+	
+	
+}
